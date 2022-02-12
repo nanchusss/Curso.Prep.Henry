fcaaff940b767b3206e25f299a3ce54bdb8a85b1
Nancy <nancyart83@gmail.com>
2022-02-12 19:57:47 -0300

    Primer Trabajo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -73,7 +73,8 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  return str1.length === str2.lenght;
+  
+  return str1.length === str2.length;
 }
 
 
@@ -239,15 +240,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.lenght >1) {
+  if (letra.length > 1 ) {
     return "Dato Incorrecto";
   } 
   if (letra === "a"| letra === "e" | letra === "i" | letra === "o" | letra === "u") {
-    return "Es vocal";
-  } else {
-    return "Dato Incorrecto";
+    return "Es vocal"; }
+    return "Es incorrecto";
   }
-}
+
 
 
 
