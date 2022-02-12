e1fb8b9cc4d331134a712d91a91dfcbd51516c29
Nancy <nancyart83@gmail.com>
2022-02-12 20:07:03 -0300

    Primer Trabajo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -240,11 +240,11 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
-  if (letra.length > 1 ) {
+
+  if(letra.length > 1 ){
     return "Dato Incorrecto";
   } 
-  if (letra === "a"| letra === "e" | letra === "i" | letra === "o" | letra === "u") {
+  if(letra === "a"| letra === "e" | letra === "i" | letra === "o" | letra === "u") {
     return "Es vocal"; }
   {
     return "Dato incorrecto";
