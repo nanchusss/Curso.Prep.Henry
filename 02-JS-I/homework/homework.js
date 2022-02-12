141beb8adb207342734ba4b4a6a2c3a4abac002f
Nancy <nancyart83@gmail.com>
2022-02-12 12:38:21 -0300

    nn

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -155,19 +155,19 @@ function elevar(num, exponent) {
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  
+  return Math.round(num);
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  return Math.ceil(num)
+  return Math.ceil(num);
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  return Math.random()
+  return Math.random();
   
 }
 
@@ -183,7 +183,8 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-} return str + "!";
+ return str + "!";
+}
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
@@ -237,7 +238,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra.lenght >1) {
+    return "Dato Incorrecto";
+  } 
+  if (letra ===a,e,i,o,u) {
+    return "Es vocal";
+  } else {
+    return "Dato Incorrecto";
+  }
 }
 
 
