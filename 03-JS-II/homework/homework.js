8dc6d262be96d352e335c0dace2a74258af0d1a6
Nancy <nancyart83@gmail.com>
2022-02-14 18:52:23 -0300

    Segunda Tarea

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -111,10 +111,8 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (numero <= 50 && numero >= 20){
-    return true
-  } else if (numero >= 50 || numero <= 20 ) {
-    return false
+  if (numero <= 50 && numero >= 20) {
+    return true;
   }
 }
 
