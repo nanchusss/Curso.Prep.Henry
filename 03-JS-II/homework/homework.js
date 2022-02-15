baa48b7ddacaf34d6b2b7d0f4d555cab6ac200a1
Nancy <nancyart83@gmail.com>
2022-02-15 15:09:00 -0300

    Segunda Tarea

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -168,15 +168,15 @@ function operadoresLogicos(num1, num2, num3) {
   {
     return "Número 1 es mayor y positivo" 
   }
-  else if (num1 < 0 || num2 < 0 || num3 < 0) { 
+  if (num1 < 0 || num2 < 0 || num3 < 0) { 
    
     return "Hay negativos"
   }
-  else if (num3 > num1 && num3 > num2)
+  if (num3 > num1 && num3 > num2)
    {
     return (num3++)
   }
-  else if (num1 === 0 || num2 === 0 || num3 === 0 )
+  if (num1 === 0 || num2 === 0 || num3 === 0 )
 {
   return "Error"
 } else {
