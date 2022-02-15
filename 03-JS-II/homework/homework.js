4f0d350778bc2d85ec3ae0d838375bd3be1a4969
Nancy <nancyart83@gmail.com>
2022-02-15 19:53:27 -0300

    Segunda Tarea

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -219,8 +219,7 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
- 
-  var tabla0,6,12]; 
+}
 
   
   
@@ -229,13 +228,17 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if (numero<=99 && numero<=1000);
-  {
+  if (!(numero <= 99 || numero >= 1000 ))
+  { 
     return true
-  } else {
-    return false
-  }
+} 
+  else {
+
+  return false
 }
+}
+
+
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
