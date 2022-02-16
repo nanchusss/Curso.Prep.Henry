ff8c022eedd53b94c3ff00a96ef3c4e1160c7e9f
Nancy <nancyart83@gmail.com>
2022-02-16 15:37:27 -0300

    Tercer Homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -70,9 +70,7 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-  var array = [1,"elemento"];
-if(array.includes("elemento")); {
-  return "true" }
+ return array.includes("elemento");
   }
 
 
