63cdd86d760a42506deea40452632cf20f6da8a2
Nancy <nancyart83@gmail.com>
2022-02-16 10:52:26 -0300

    Tercer Homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -40,7 +40,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-  return array.push(elemento);
+  array.push(elemento);
+  return array;
 }
 
 
