ebe547186df2eabffcd19feae63e4d704e2e4f90
Nancy <nancyart83@gmail.com>
2022-02-17 16:50:04 -0300

    Tercer Homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -130,10 +130,10 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo) {
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-  
+
   for ( var i= 0; i= arreglo.length; i++) {
     
-    if(arreglo.length >= 18) 
+    if(arreglo[i] >= 18) 
 
     return arreglo.length
   }
