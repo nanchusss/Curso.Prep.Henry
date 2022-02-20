b86e6e583e58dd178388640dfe352640b0b36832
Nancy <nancyart83@gmail.com>
2022-02-20 01:28:23 -0300

    Cuarto Homework

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,15 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+
+  var nombre = {
+    nombre: "nombre",
+    edad : "edad",
+    meow: function() {
+      return "Meow!"
+        }
+  }
+  return nombre
 }
 
 
