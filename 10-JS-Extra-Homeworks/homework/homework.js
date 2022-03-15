c1ec4538f8b6715cea0b81badd050ef321600083
Nancy <nancyart83@gmail.com>
2022-03-15 12:51:38 -0300

    Extra Homework

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+
+  var arrayNuevo = Object.keys(objeto); 
+  return arrayNuevo;
+  
+  
 }
 
 
